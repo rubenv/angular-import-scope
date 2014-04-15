0423183f068cc0489819bc7467168b0dbe4b11a5
Ruben Vermeersch <ruben@rocketeer.be>
2014-04-15 13:57:36 +0200

    More browser configurations.

--- test-config.js
+++ test-config.js
@@ -1,10 +1,16 @@
-function testBrowser(name) {
-    return {
+function testBrowser(name, version) {
+    var config {
         'browserName': name,
         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER || 'test',
         'build': process.env.TRAVIS_BUILD_NUMBER,
         'name': 'angular-import-scope'
     }
+
+    if (version) {
+        config.version = version;
+    }
+
+    return config;
 }
 
 exports.config = {
@@ -15,7 +21,15 @@ exports.config = {
     multiCapabilities: [
         testBrowser('chrome'),
         testBrowser('firefox'),
-        testBrowser('safari'),
-        testBrowser('internet explorer'),
+        testBrowser('safari', 5),
+        testBrowser('safari', 6),
+        testBrowser('safari', 7),
+        testBrowser('internet explorer', 9),
+        testBrowser('internet explorer', 10),
+        testBrowser('internet explorer', 11),
+        testBrowser('iphone', '6.1'),
+        testBrowser('iphone', '7.1'),
+        testBrowser('android', '4.0'),
+        testBrowser('android', '4.3'),
     ]
 };
