55308fc7e88fb8f71d59d79965bf0a84d09d23ba
Ruben Vermeersch <ruben@rocketeer.be>
2014-12-09 16:51:41 +0100

    Expose findScope as a service.

--- dist/angular-import-scope.js
+++ dist/angular-import-scope.js
@@ -1,63 +1,65 @@
-angular.module("rt.importscope", ["ui.router"]).directive("importScope", ["$compile", "$animate", "$rootElement", "$timeout", function ($compile, $animate, $rootElement, $timeout) {
-    // Trim polyfill for old browsers (instead of jQuery)
-    // Based on AngularJS-v1.2.2 (angular.js#620)
-    var trim = (function () {
-        if (!String.prototype.trim) {
-            return function (value) {
-                return (typeof value === "string") ? value.replace(/^\s*/, "").replace(/\s*$/, "") : value;
-            };
-        }
-        return function (value) {
-            return (typeof value === "string") ? value.trim() : value;
-        };
-    })();
+angular.module("rt.importscope", ["ui.router"])
+    .factory("findScope", ["$rootElement", function ($rootElement) {
+        var root = angular.element($rootElement);
 
-    var root = angular.element($rootElement);
-
-    function findScope(name) {
-        var views = root.find("ui-view, [ui-view]");
-        for (var i = 0; i < views.length; i++) {
-            var el = angular.element(views[i]);
-            var data = el.data("$uiView");
-            if (data && data.name === name) {
-                return el.children().scope();
-            }
-        }
-        return null;
-    }
-
-    return {
-        terminal: true,
-        compile: function compile(el) {
-            var content = trim(el.html());
-
-            return {
-                post: function (scope, element, attrs) {
-                    // Stops the progation of the inheritedData("$uiView") call inside ui-sref.
-                    // Without this ui-sref would use an incorrect base state, which breaks relative references.
-                    el.data("$uiView", {});
-
-                    var viewName = attrs.importScope;
-                    var currentScope = null;
-
-                    function update() {
-                        var parentScope = findScope(viewName);
-                        var targetScope = parentScope || scope;
-
-                        if (targetScope !== currentScope) {
-                            var newWrapper = angular.element("<span>" + content + "</span>");
-                            $compile(newWrapper.contents())(targetScope);
-                            var oldContents = element.contents();
-                            var newContents = newWrapper.contents();
-                            $animate.enter(newContents, element);
-                            $animate.leave(oldContents);
-                            currentScope = targetScope;
-                        }
-                    }
-
-                    scope.$watch(update);
+        return function findScope(name) {
+            var views = root.find("ui-view, [ui-view]");
+            for (var i = 0; i < views.length; i++) {
+                var el = angular.element(views[i]);
+                var data = el.data("$uiView");
+                if (data && data.name === name) {
+                    return el.children().scope();
                 }
+            }
+            return null;
+        };
+    }])
+    .directive("importScope", ["$compile", "$animate", "$rootElement", "$timeout", "findScope", function ($compile, $animate, $rootElement, $timeout, findScope) {
+        // Trim polyfill for old browsers (instead of jQuery)
+        // Based on AngularJS-v1.2.2 (angular.js#620)
+        var trim = (function () {
+            if (!String.prototype.trim) {
+                return function (value) {
+                    return (typeof value === "string") ? value.replace(/^\s*/, "").replace(/\s*$/, "") : value;
+                };
+            }
+            return function (value) {
+                return (typeof value === "string") ? value.trim() : value;
             };
-        }
-    };
-}]);
+        })();
+
+        return {
+            terminal: true,
+            compile: function compile(el) {
+                var content = trim(el.html());
+
+                return {
+                    post: function (scope, element, attrs) {
+                        // Stops the progation of the inheritedData("$uiView") call inside ui-sref.
+                        // Without this ui-sref would use an incorrect base state, which breaks relative references.
+                        el.data("$uiView", {});
+
+                        var viewName = attrs.importScope;
+                        var currentScope = null;
+
+                        function update() {
+                            var parentScope = findScope(viewName);
+                            var targetScope = parentScope || scope;
+
+                            if (targetScope !== currentScope) {
+                                var newWrapper = angular.element("<span>" + content + "</span>");
+                                $compile(newWrapper.contents())(targetScope);
+                                var oldContents = element.contents();
+                                var newContents = newWrapper.contents();
+                                $animate.enter(newContents, element);
+                                $animate.leave(oldContents);
+                                currentScope = targetScope;
+                            }
+                        }
+
+                        scope.$watch(update);
+                    }
+                };
+            }
+        };
+    }]);
